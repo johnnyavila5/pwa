a0cfa468fb06bf3a3c4f8be6d17da0ddb2f3d589
Jonathan Avila <johnnyavila5@aol.com>
2019-02-26 02:00:24 +0000

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>About Plants</h1>
+    <img
+        src="https://3c1703fe8d.site.internapcdn.net/newman/gfx/news/hires/2018/plant.jpg"
+        alt="A Cactus"
+      />
+    <p>
+      Florem ipsum spurge crocus september flower snowdrop. Sweet William windflower, eustoma floss flower oxlip wormwood. Spotted rock-rose guernsey lily larkspur. Sweet sultan peony sugarbush brodiaea. Sunflower sneezeweed lisianthus saxifrage scabious honeysuckle. Forget-me-not twinflower love lies bleeding foxglove, queen Anne’s lace.
+
+Gorse scottish dock daisy. Mimosa spring gentian royal bluebell windflower canterbury bells feverfew. Snow on the mountain houndstongue lady’s mantle scots primrose, spurge. Sweet William anemone, queen Fabiola lily slipper orchid oxlip eustoma. Spray carnation tjenkenrientjee september flower cowslip blue throatwort sugarbush statice. Flame lily.
+
+Golden shower orchid, arum goosefoot tuberose chincerinchee scarlet plume ox-eye daisy. Drumstick acacia september flower dandelion petal carnation red-hot poker. China aster bearberry scorpion orchid lily radnor lily scots primrose. Pasqueflower mimosa bloody crane's-bill, daffodil. Blossom, baby’s breath poppy primrose singapore orchid. Flower flamingo flower canterbury bells heather.
+    </p>
+  </Layout>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <h1>Amazing Plants</h1>
+    <div>
+      <img
+        src="http://www.potterybarn.com.au/core/media/media.nl?id=64365259&c=3572911&h=afca07656478e08c28f9&resizeid=4&resizeh=1200&resizew=1200"
+        alt="A Cactus"
+      />
+    </div>
+  </Layout>
+)
